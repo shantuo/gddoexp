efd88d8062ad8a49419cd9b76d6ff675b18d5399
Rafael Dantas Justo <rafael@registro.br>
2015-10-08 09:17:17 -0300

    Add tests for new services

--- gddoexp_test.go
+++ gddoexp_test.go
@@ -305,7 +305,7 @@ func TestShouldArchivePackage(t *testing.T) {
 			if item.expected {
 				t.Errorf("[%d] %s: expected package to be archived", i, item.description)
 			} else {
-				t.Errorf("[%d] %s: expected package to be don't be archived", i, item.description)
+				t.Errorf("[%d] %s: expected package to don't be archived", i, item.description)
 			}
 		}
 
@@ -403,7 +403,7 @@ func TestShouldArchivePackages(t *testing.T) {
 			},
 			httpClient: httpClientMock{
 				getMock: func(url string) (*http.Response, error) {
-					if !strings.HasSuffix(url, "client_id=exampleuser&client_secret=abc123") {
+					if !strings.HasSuffix(url, "?client_id=exampleuser&client_secret=abc123") {
 						return &http.Response{
 							StatusCode: http.StatusBadRequest,
 						}, nil
@@ -532,7 +532,601 @@ func TestShouldArchivePackages(t *testing.T) {
 			responses = append(responses, response)
 		}
 
-		sort.Sort(byPath(responses))
+		sort.Sort(byArchiveResponsePath(responses))
+		if !reflect.DeepEqual(item.expected, responses) {
+			t.Errorf("[%d] %s: mismatch responses.\n%v", i, item.description, diff(item.expected, responses))
+		}
+	}
+}
+
+func TestIsFastForkPackage(t *testing.T) {
+	data := []struct {
+		description   string
+		path          string
+		auth          *gddoexp.GithubAuth
+		httpClient    httpClientMock
+		expected      bool
+		expectedCache bool
+		expectedError error
+	}{
+		{
+			description: "it should detect a fast fork package (without authentication)",
+			path:        "github.com/rafaeljusto/dns",
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if url == "https://api.github.com/repos/rafaeljusto/dns" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+
+					} else if url == "https://api.github.com/repos/rafaeljusto/dns/commits" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusBadRequest,
+						}, nil
+					}
+				},
+			},
+			expected: true,
+		},
+		{
+			description: "it should detect a fast fork package (authenticated)",
+			path:        "github.com/rafaeljusto/dns",
+			auth: &gddoexp.GithubAuth{
+				ID:     "exampleuser",
+				Secret: "abc123",
+			},
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if url == "https://api.github.com/repos/rafaeljusto/dns?client_id=exampleuser&client_secret=abc123" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+
+					} else if url == "https://api.github.com/repos/rafaeljusto/dns/commits?client_id=exampleuser&client_secret=abc123" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusBadRequest,
+						}, nil
+					}
+				},
+			},
+			expected: true,
+		},
+		{
+			description: "it should fail when there's a HTTP problem with Github API (repo request)",
+			path:        "github.com/rafaeljusto/gddoexp",
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					return nil, fmt.Errorf("i'm a crazy error")
+				},
+			},
+			expectedError: gddoexp.NewError("github.com/rafaeljusto/gddoexp", gddoexp.ErrorCodeGithubFetch, fmt.Errorf("i'm a crazy error")),
+		},
+		{
+			description: "it should detect that is not a fast fork when the repository isn't a fork",
+			path:        "github.com/miekg/dns",
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if url == "https://api.github.com/repos/miekg/dns" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": false
+}`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusBadRequest,
+						}, nil
+					}
+				},
+			},
+			expected: false,
+		},
+		{
+			description: "it should fail when there's a HTTP problem with Github API (commits request)",
+			path:        "github.com/rafaeljusto/dns",
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if url == "https://api.github.com/repos/rafaeljusto/dns" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+					}
+
+					return nil, fmt.Errorf("i'm a crazy error")
+				},
+			},
+			expectedError: gddoexp.NewError("github.com/rafaeljusto/dns", gddoexp.ErrorCodeGithubFetch, fmt.Errorf("i'm a crazy error")),
+		},
+		{
+			description: "it should detect that is not a fast fork when there's a commit after the commits period",
+			path:        "github.com/rafaeljusto/dns",
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if url == "https://api.github.com/repos/rafaeljusto/dns" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+
+					} else if url == "https://api.github.com/repos/rafaeljusto/dns/commits" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  },
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-11T21:56:24Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusBadRequest,
+						}, nil
+					}
+				},
+			},
+			expected: false,
+		},
+		{
+			description: "it should detect that is not a fast fork when there are too many commits ",
+			path:        "github.com/rafaeljusto/dns",
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if url == "https://api.github.com/repos/rafaeljusto/dns" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+
+					} else if url == "https://api.github.com/repos/rafaeljusto/dns/commits" {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  },
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:01:00Z"
+      }
+    }
+  },
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:02:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusBadRequest,
+						}, nil
+					}
+				},
+			},
+			expected: false,
+		},
+		{
+			description:   "it should fail when it's not a Github project",
+			path:          "bitbucket.org/rafaeljusto/gddoexp",
+			expectedCache: true,
+			expectedError: gddoexp.NewError("bitbucket.org/rafaeljusto/gddoexp", gddoexp.ErrorCodeNonGithub, nil),
+		},
+	}
+
+	httpClientBkp := gddoexp.HTTPClient
+	defer func() {
+		gddoexp.HTTPClient = httpClientBkp
+	}()
+
+	isCacheResponseBkp := gddoexp.IsCacheResponse
+	defer func() {
+		gddoexp.IsCacheResponse = isCacheResponseBkp
+	}()
+	gddoexp.IsCacheResponse = func(r *http.Response) bool {
+		return r.Header.Get("Cache") == "1"
+	}
+
+	for i, item := range data {
+		gddoexp.HTTPClient = item.httpClient
+
+		p := database.Package{
+			Path: item.path,
+		}
+
+		fastFork, cache, err := gddoexp.IsFastForkPackage(p, item.auth)
+
+		if fastFork != item.expected {
+			if item.expected {
+				t.Errorf("[%d] %s: expected package to be a fast fork", i, item.description)
+			} else {
+				t.Errorf("[%d] %s: expected package to don't be a fast fork", i, item.description)
+			}
+		}
+
+		if cache != item.expectedCache {
+			if item.expectedCache {
+				t.Errorf("[%d] %s: expected hit in cache", i, item.description)
+			} else {
+				t.Errorf("[%d] %s: unexpected hit in cache", i, item.description)
+			}
+		}
+
+		if !reflect.DeepEqual(item.expectedError, err) {
+			t.Errorf("[%d] %s: expected error to be “%v” and got “%v”", i, item.description, item.expectedError, err)
+		}
+	}
+}
+
+func TestAreFastForkPackages(t *testing.T) {
+	data := []struct {
+		description string
+		packages    []database.Package
+		auth        *gddoexp.GithubAuth
+		httpClient  httpClientMock
+		expected    []gddoexp.FastForkResponse
+	}{
+		{
+			description: "it should detect that all packages are fast fork (without authentication)",
+			packages: []database.Package{
+				{Path: "github.com/rafaeljusto/dns"},
+				{Path: "github.com/rafaeljusto/go-testdb"},
+				{Path: "github.com/rafaeljusto/mysql"},
+				{Path: "github.com/rafaeljusto/handy"},
+				{Path: "github.com/rafaeljusto/schema"},
+			},
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if strings.HasSuffix(url, "/commits") {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+					}
+				},
+			},
+			expected: []gddoexp.FastForkResponse{
+				{
+					Path:     "github.com/rafaeljusto/dns",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/go-testdb",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/handy",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/mysql",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/schema",
+					FastFork: true,
+				},
+			},
+		},
+		{
+			description: "it should detect that all packages are fast fork (authenticated)",
+			packages: []database.Package{
+				{Path: "github.com/rafaeljusto/dns"},
+				{Path: "github.com/rafaeljusto/go-testdb"},
+				{Path: "github.com/rafaeljusto/mysql"},
+				{Path: "github.com/rafaeljusto/handy"},
+				{Path: "github.com/rafaeljusto/schema"},
+			},
+			auth: &gddoexp.GithubAuth{
+				ID:     "exampleuser",
+				Secret: "abc123",
+			},
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if !strings.HasSuffix(url, "?client_id=exampleuser&client_secret=abc123") {
+						return &http.Response{
+							StatusCode: http.StatusBadRequest,
+						}, nil
+					}
+
+					url = strings.TrimSuffix(url, "?client_id=exampleuser&client_secret=abc123")
+
+					if strings.HasSuffix(url, "/commits") {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+					}
+				},
+			},
+			expected: []gddoexp.FastForkResponse{
+				{
+					Path:     "github.com/rafaeljusto/dns",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/go-testdb",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/handy",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/mysql",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/schema",
+					FastFork: true,
+				},
+			},
+		},
+		{
+			description: "it should detect that all packages are fast fork (cache hits)",
+			packages: []database.Package{
+				{Path: "github.com/rafaeljusto/dns"},
+				{Path: "github.com/rafaeljusto/go-testdb"},
+				{Path: "github.com/rafaeljusto/mysql"},
+				{Path: "github.com/rafaeljusto/handy"},
+				{Path: "github.com/rafaeljusto/schema"},
+			},
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if strings.HasSuffix(url, "/commits") {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Header: http.Header{
+								"Cache": []string{"1"},
+							},
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Header: http.Header{
+								"Cache": []string{"1"},
+							},
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+					}
+				},
+			},
+			expected: []gddoexp.FastForkResponse{
+				{
+					Path:     "github.com/rafaeljusto/dns",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/go-testdb",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/handy",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/mysql",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/schema",
+					FastFork: true,
+				},
+			},
+		},
+		{
+			description: "it should detect that all packages are fast fork (partial cache hits)",
+			packages: []database.Package{
+				{Path: "github.com/rafaeljusto/dns"},
+				{Path: "github.com/rafaeljusto/go-testdb"},
+				{Path: "github.com/rafaeljusto/mysql"},
+				{Path: "github.com/rafaeljusto/handy"},
+				{Path: "github.com/rafaeljusto/schema"},
+			},
+			httpClient: httpClientMock{
+				getMock: func(url string) (*http.Response, error) {
+					if strings.HasSuffix(url, "/commits") {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body: ioutil.NopCloser(bytes.NewBufferString(`[
+  {
+    "commit": {
+      "author": {
+        "date": "2010-08-03T23:00:00Z"
+      }
+    }
+  }
+]`)),
+						}, nil
+
+					} else {
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Header: http.Header{
+								"Cache": []string{"1"},
+							},
+							Body: ioutil.NopCloser(bytes.NewBufferString(`{
+  "created_at": "2010-08-03T21:56:23Z",
+  "fork": true
+}`)),
+						}, nil
+					}
+				},
+			},
+			expected: []gddoexp.FastForkResponse{
+				{
+					Path:     "github.com/rafaeljusto/dns",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/go-testdb",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/handy",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/mysql",
+					FastFork: true,
+				},
+				{
+					Path:     "github.com/rafaeljusto/schema",
+					FastFork: true,
+				},
+			},
+		},
+	}
+
+	httpClientBkp := gddoexp.HTTPClient
+	defer func() {
+		gddoexp.HTTPClient = httpClientBkp
+	}()
+
+	isCacheResponseBkp := gddoexp.IsCacheResponse
+	defer func() {
+		gddoexp.IsCacheResponse = isCacheResponseBkp
+	}()
+	gddoexp.IsCacheResponse = func(r *http.Response) bool {
+		return r.Header.Get("Cache") == "1"
+	}
+
+	rateLimitBkp := gddoexp.RateLimit
+	defer func() {
+		gddoexp.RateLimit = rateLimitBkp
+	}()
+
+	// Change rate limit parameters to speed up the test
+	gddoexp.RateLimit.FillInterval = time.Millisecond
+	gddoexp.RateLimit.Capacity = 100
+	gddoexp.RateLimit.AuthFillInterval = time.Millisecond
+	gddoexp.RateLimit.AuthCapacity = 100
+
+	for i, item := range data {
+		gddoexp.HTTPClient = item.httpClient
+
+		var responses []gddoexp.FastForkResponse
+		for response := range gddoexp.AreFastForkPackages(item.packages, item.auth) {
+			responses = append(responses, response)
+		}
+
+		sort.Sort(byFastForkResponsePath(responses))
 		if !reflect.DeepEqual(item.expected, responses) {
 			t.Errorf("[%d] %s: mismatch responses.\n%v", i, item.description, diff(item.expected, responses))
 		}
@@ -562,8 +1156,14 @@ func diff(a, b interface{}) []difflib.DiffRecord {
 	)
 }
 
-type byPath []gddoexp.ArchiveResponse
+type byArchiveResponsePath []gddoexp.ArchiveResponse
 
-func (b byPath) Len() int           { return len(b) }
-func (b byPath) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
-func (b byPath) Less(i, j int) bool { return b[i].Path < b[j].Path }
+func (b byArchiveResponsePath) Len() int           { return len(b) }
+func (b byArchiveResponsePath) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byArchiveResponsePath) Less(i, j int) bool { return b[i].Path < b[j].Path }
+
+type byFastForkResponsePath []gddoexp.FastForkResponse
+
+func (b byFastForkResponsePath) Len() int           { return len(b) }
+func (b byFastForkResponsePath) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byFastForkResponsePath) Less(i, j int) bool { return b[i].Path < b[j].Path }
